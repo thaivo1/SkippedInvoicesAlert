4e4abe92f233ffe1119746928081abafa423ad3c
Thai.Vo <thai.vo@chadwellsupply.com>
2020-04-27 15:39:19 -0400

    Add project files.

---
+++ SkippedInvoicesAlert/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SkippedInvoicesAlert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
