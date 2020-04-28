a91753f5ab306d41380c3fbb009288f8a9562c46
Thai.Vo <thai.vo@chadwellsupply.com>
2020-04-28 08:56:22 -0400

    Simple mockup

--- SkippedInvoicesAlert/Program.cs
+++ SkippedInvoicesAlert/Program.cs
@@ -6,7 +6,43 @@ namespace SkippedInvoicesAlert
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World2!"); 
+            Console.WriteLine("SkippedInvoicesAlert project starts");
+
+            string sInvoiceData = GetInvoiceData();
+
+            string sEmail = SendEmailToCustomers();
+
+
+
+            Console.WriteLine("Press any key to continue.");
+            Console.Read();
+
+        }
+
+        public static string GetInvoiceData()
+        {
+            Console.WriteLine("Start GetInvoiceData()");
+
+            Console.WriteLine("End GetInvoiceData()");
+            return "Done get data";
+        }
+
+        public static string SendEmailToCustomers()
+        {
+            Console.WriteLine("Start SendEmailToCustomers()");
+
+            Console.WriteLine("End SendEmailToCustomers()");
+            return "Done sending email";
+        }
+
+        public static string RecordActionsToDatabase()
+        {
+            Console.WriteLine("Start RecordActionsToDatabase()");
+
+            Console.WriteLine("End RecordActionsToDatabase()");
+            return "Done record action to database";
         }
     }
+
+
 }
